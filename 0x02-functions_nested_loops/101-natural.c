0246bffa65f2a5f7600c43dea0ae200b845bff81
Emmanuel Inyang Williams <98588650+ofiankor@users.noreply.github.com>
2022-06-12 08:56:11 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,21 +3,16 @@
  *  * main - Prints natural numbers below 1024 that are
  *   * multiplies of 3 or 
  *     * Return: Always 0.
- *
  */
-
 int main(void)
-
 {
 	int a, b;
 
 	for (a = 1; a < 1024; a++)
-		{		
-			if ((a % 3) == 0 || (a % 5) == 0)
-
-				b += a;
-		}
+	{		
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
 	printf("%d\n", b);
 	return (0);
-
 }
