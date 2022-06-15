a66ad6a43626636c00f12f052e982304c2ab6f0b
ofiankor <elchino4594@gmail.com>
2022-06-15 23:22:10 +0100

    put half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,22 @@
 include "main.h"
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
+}
+
+/**
  * puts_half - prints half of a string.
  * @str: input string.
  * Return: no return.
