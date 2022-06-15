be8112b6c514f85b7f127349cb9b49e52fddbcdd
ofiankor <elchino4594@gmail.com>
2022-06-15 23:20:08 +0100

    put half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,20 +6,16 @@ include "main.h"
  */
 void puts_half(char *str)
 {
-	int count = 0, i;
-	while (count >= 0)
-	{
-		if (str[count] == '\0')
-			break;
-		count++;
-	}
-
-	if (count % 2 == 1)
-		i = count / 2;
-	else
-		i = (count - 1) / 2;
-
-	for (i++; i < count; i++)
-		_putchar(str[i]);
-	_putchar('\n');
+int i;
+int length = _strlen(str);
+if (length % 2 != 0)
+i = (length + 1) / 2;
+else
+i = (length / 2);
+while (i < length)
+{
+_putchar(*(str + i));
+i++;
+}
+_putchar('\n');
 }
